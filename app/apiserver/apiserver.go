7693b095bec757d48e8eb59c9e9f5fc16af5858f
gleb <hlib.dikariev@ultrano.pl>
2020-02-11 20:54:43 +0100

    http server config/logger

---
+++ app/apiserver/apiserver.go
@@ -0,0 +1,37 @@
+package apiserver
+
+// ApiServer ... 
+type ApiServer struct {
+	config *Config
+	logger *logrus.Logger
+}
+
+// New ...
+func New(config *Config) *ApiServer  {
+	return &ApiServer{
+		config: config
+		logger: logrus.New()
+	}
+}
+
+// Start ...
+func (s *ApiServer) Start() error {
+	if err != s.configureLogger; err != nill {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *ApiServer) configureLogger() error {
+	level, err != logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module golang_rest
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+)
