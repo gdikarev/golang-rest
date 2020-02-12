43b0fef2f528d6fbc6c136c258cae85cd874fc71
gleb <hlib.dikariev@ultrano.pl>
2020-02-12 14:58:06 +0000

    delete accidenty commited file

--- main.go
+++ main.go
@@ -1,32 +1,34 @@
 package main
 
 import (
-    "flag"
-    "log"
-    "github.com/BurntSushi/toml"
-    "github.com/gdikarev/golang-rest/app/apiserver"
+	"flag"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/gdikarev/golang-rest/app/apiserver"
 )
 
 var (
-    configPath string
+	configPath string
 )
 
 func init() {
-    flag.StringVar(&configPath, "config-path", "config/apiserver.toml", "path to config file")
+	flag.StringVar(&configPath, "config-path", "config/apiserver.toml", "path to config file")
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    config := apiserver.NewConfig()
-    _, err := toml.DecodeFile(configPath, config)
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    s := apiserver.New(config)
-    if err:= s.Start(); err != nil {
-        log.Fatal(err)
-    }
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+
+	}
 }
