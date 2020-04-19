2c12e6c7d9ecfba7bead0025a6ecad18dce19e50
gleb <hlib.dikariev@ultrano.pl>
2020-04-19 10:00:50 +0000

    initial tests for user repository

---
+++ app/migrations/2_add_uuid_employees.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE employees ADD COLUMN uuid CHAR(36)
--- app/model/employee.go
+++ app/model/employee.go
@@ -2,6 +2,7 @@ package model
 
 type Employee struct {
 	ID         int64
+	UUID       string
 	FistName   string
 	SecondName string
 	LastName   string
